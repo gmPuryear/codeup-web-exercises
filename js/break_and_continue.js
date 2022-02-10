c148446e2615be4a05b248259c6630918d00c69a
George Puryear <gm.puryear@yahoo.com>
2022-02-10 17:10:48 -0600

    modified break_and_continue.js and for_loops.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,31 @@
 "use strict"
 //
+//
 //                                  *** Break and Continue ***
 do {
     var userNumberChoice = prompt("Please enter an odd number between 1 and 50");
 } while (userNumberChoice % 2 === 0 || userNumberChoice < 1 || userNumberChoice > 50);
 for (var i = 1; i <= 50; i += 2) {
-    if (i === userNumberChoice) {
+    if (i == userNumberChoice) {
         console.log("Yikes! Skipping number: " + userNumberChoice);
-        continue
-    } else {
-        console.log("Here is an odd number: " + i);
+        continue;
     }
+    console.log("Here is an odd number: " + i);
 }
+
+// Not using web
+
+// function breakAndContinue(num) {
+//     do {
+//     } while (num % 2 === 0 || num < 1 || num > 50);
+//     for (var i = 1; i <= 50; i += 2) {
+//         console.log("Here is an odd number: " + i);
+//         if (i === num) {
+//             console.log("Yikes! Skipping number: " + num);
+//             continue;
+//         }
+//     }
+// }
+
+// let num = 45
+// breakAndContinue(num);
--- js/for_loops.js
+++ js/for_loops.js
@@ -26,6 +26,11 @@
 //         console.log(space += j);
 //     }
 // }
+for (var i = 1; i <= 9; i++) {
+    for (var j = 1; j <= i; j++) {
+        console.log();
+    }
+}
 
 
 // for (var i = 100; i >= 5; i -= 5) {
