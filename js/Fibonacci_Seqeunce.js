52839e774ebbe27481622614433f65b354bf8fb0
George Puryear <gm.puryear@yahoo.com>
2022-02-11 09:27:32 -0600

    added Fibonacci_Sequence.js and modified break_and_continue.js

---
+++ js/Fibonacci_Seqeunce.js
@@ -0,0 +1,5 @@
+for (var i = 1; i <= 50; i++) {
+    let twoBack = i - 2;
+    let oneBack = i - 1;
+    console.log(twoBack + oneBack);
+}
--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,5 @@
 "use strict"
 //
-//
 //                                  *** Break and Continue ***
 do {
     var userNumberChoice = prompt("Please enter an odd number between 1 and 50");
@@ -12,20 +11,3 @@ for (var i = 1; i <= 50; i += 2) {
     }
     console.log("Here is an odd number: " + i);
 }
-
-// Not using web
-
-// function breakAndContinue(num) {
-//     do {
-//     } while (num % 2 === 0 || num < 1 || num > 50);
-//     for (var i = 1; i <= 50; i += 2) {
-//         console.log("Here is an odd number: " + i);
-//         if (i === num) {
-//             console.log("Yikes! Skipping number: " + num);
-//             continue;
-//         }
-//     }
-// }
-
-// let num = 45
-// breakAndContinue(num);
