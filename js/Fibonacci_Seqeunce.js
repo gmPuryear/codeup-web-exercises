6f2e5a4e9548d34c58f630b7e498480964ac2c87
George Puryear <gm.puryear@yahoo.com>
2022-02-14 09:46:54 -0600

    ModifiedFibonacci_Sequence.js and loops.html

--- js/Fibonacci_Seqeunce.js
+++ js/Fibonacci_Seqeunce.js
@@ -1,5 +1,15 @@
-for (var i = 1; i <= 50; i++) {
-    let twoBack = i - 2;
-    let oneBack = i - 1;
-    console.log(twoBack + oneBack);
+"use strict"
+//                                            *** Fibonacci Numbers ***
+// Write a loop that outputs the first 50 fibonacci numbers. https://en.wikipedia.org/wiki/Fibonacci_number
+// Recommend starting your loop at 1 and ending your loop once you've calculated 50 fibonacci numbers.
+
+var arr = [0, 1];
+for (var i = 2; i < 50; i++) {
+    if (i === 2) {
+        arr.push(arr[i - 1]);
+    } else {
+        arr.push(arr[i - 1] + arr[i - 2]);
+    }
 }
+console.log(arr);
+console.log(arr.length);
