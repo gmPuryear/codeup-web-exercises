78a36d9e66fb9b66d23ed3f6cf6cef6de4780dcb
George Puryear <gm.puryear@yahoo.com>
2022-02-14 16:46:17 -0600

    modified warmups.js

--- js/warmups.js
+++ js/warmups.js
@@ -12,3 +12,24 @@ function findAverage(arr) {
 }
 console.log(findAverage([95, 74, 86, 100]));
 
+var car = { make: "ford", model: "F-150", Color: "blue"};
+    console.log(car);
+    car['engine'] = 5.3 + 'l';
+    console.log(car);
+    car['model'] = "Mustang";
+    console.log(car);
+
+    let arr = [50, 2, 40, 60];
+    let min = Math.min(arr);
+    console.log(min);
+
+    let current = arr[0];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] === current) {
+            continue;
+        } if (arr[i] < arr[0]) {
+            var low = arr[i];
+        }
+
+    }
+    console.log(low);
