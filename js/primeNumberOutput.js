e57c43d2f8ce6e6f5e1fa137f90ea088d7911adc
George Puryear <gm.puryear@yahoo.com>
2022-02-15 10:50:03 -0600

    added: circle.js, primeNumberOutput.js, math-js.html

---
+++ js/primeNumberOutput.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// Write the code necessary to output the first 50 prime numbers
+// Recommend starting your loop at 1 and ending your loop once you've calcualted 50 primes.
+
+for (let i = 1; i < 51; i++) {
+    // if i divided by
+}
