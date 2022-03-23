741ea78bb7d80883d163e54b2b6e54d04633cc8c
George Puryear <gm.puryear@yahoo.com>
2022-03-23 09:45:49 -0500

    finished exercise practicing map, filter, reduce methods

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -83,20 +83,22 @@ let languages = users.reduce((listOfLanguages, user) => {
 }, [])
 console.log(languages);
 
-// const people = [
-//     {name: "Kyle", age: 26},
-//     {name: "John", age: 31},
-//     {name: "Sally", age: 42},
-//     {name: "Jill", age: 42}
-// ]
-//
-// const ageGroups = people.reduce ((groupedPeople, person) => {
-//     const age = person.age;
-//     if (groupedPeople[age] == null)
-//         groupedPeople[age] = []
-//         groupedPeople[age].push(person)
-//         return groupedPeople
-// },[])
-// console.log(ageGroups);
+
+// .reduce Practice
+const people = [
+    {name: "Kyle", age: 26},
+    {name: "John", age: 31},
+    {name: "Sally", age: 42},
+    {name: "Jill", age: 42}
+]
+
+const ageGroups = people.reduce ((groupedPeople, person) => {
+    const age = person.age;
+    if (groupedPeople[age] == null)
+        groupedPeople[age] = []
+        groupedPeople[age].push(person)
+        return groupedPeople
+},[])
+console.log(ageGroups);
 
 
