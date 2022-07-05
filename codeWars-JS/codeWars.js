e22df13202b98deeb65b40443989419521133cea
George Puryear <gm.puryear@yahoo.com>
2022-07-05 16:50:34 -0500

    finished DNA compliment problem

--- codeWars-JS/codeWars.js
+++ codeWars-JS/codeWars.js
@@ -68,28 +68,58 @@
 // 1. Find type of CC 2. if number turn to string then split into arrau using split. 3. loop backwards starting at
 // length of array - 4 + 1, 4. change the present value of array to a #. 5. change back to string. 6. return new string.
 // ----- SOLUTION =====
-console.log(maskify(4556364607935616))
+// console.log(maskify(4556364607935616))
+//
+// function maskify(cc) {
+//     let ccToString = cc.toString();0
+//     let ccToArr = ccToString.split('');
+//     let last4Numbers = ccToArr.slice(ccToArr.length - 4, ccToArr.length);
+//     // console.log(last4Numbers);
+//     let splicedArr = ccToArr.splice(0, ccToArr.length - 4);
+//     // console.log(splicedArr);
+//     for (let i = 0; i < splicedArr.length; i++) {
+//         splicedArr[i] = '#';
+//     }
+//     // console.log(splicedArr);
+//     for (i = 0; i < last4Numbers.length; i++) {
+//         splicedArr.push(last4Numbers[i]);
+//     }
+//     // console.log(splicedArr);
+//     let answer = splicedArr.join('');
+//     console.log(answer);
+//     return answer;
+// }
 
-function maskify(cc) {
-    let ccToString = cc.toString();
-    let ccToArr = ccToString.split('');
-    let last4Numbers = ccToArr.slice(ccToArr.length - 4, ccToArr.length);
-    // console.log(last4Numbers);
-    let splicedArr = ccToArr.splice(0, ccToArr.length - 4);
-    // console.log(splicedArr);
-    for (let i = 0; i < splicedArr.length; i++) {
-        splicedArr[i] = '#';
+// ----- PROBLEM -----
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side
+// of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty
+// or there is no DNA at all (again, except for Haskell).
+// ----- PLANNING -----
+// 1. Create empty array to hold complement of the input.
+// 2. Iterate over input
+// 3. If else statements to check what character is present
+// 4. based on what that character is, push its compliment to array
+
+console.log(DNAStrand('AAAA'))
+console.log(DNAStrand('ATTGC'))
+console.log(DNAStrand('GTAT'))
+
+function DNAStrand(dna) {
+    let complimentArr = [];
+    for (let i = 0; i < dna.length; i++) {
+        // console.log(dna[i]);
+        if (dna[i] === 'A') {
+            complimentArr.push("T")
+        } else if (dna[i] === 'C') {
+            complimentArr.push("G");
+        } else if (dna[i] === "T") {
+            complimentArr.push('A')
+        } else if (dna[i] === "G") {
+            complimentArr.push("C");
+        }
     }
-    // console.log(splicedArr);
-    for (i = 0; i < last4Numbers.length; i++) {
-        splicedArr.push(last4Numbers[i]);
-    }
-    // console.log(splicedArr);
-    let answer = splicedArr.join('');
-    console.log(answer);
-    return answer;
+    let complimentArr2 = complimentArr.join('');
+    return complimentArr2;
 }
-
-
 
 
