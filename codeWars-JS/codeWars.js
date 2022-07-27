2fb3b762f9126abd4b95e4b7707746c06d755a7d
George Puryear <gm.puryear@yahoo.com>
2022-07-27 15:15:55 -0500

    completed coding challenge where the task is to find the area for a given polygon. I used the examples to figure out a pattern and I created the areas mathmatically using the Math library. So (n-1)^2 + n^2

--- codeWars-JS/codeWars.js
+++ codeWars-JS/codeWars.js
@@ -213,7 +213,28 @@
 // }
 // console.log(solution([3, 6, -2, -5, 7, 3]));
 
+// TODO: Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+//  A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by
+//  taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see
+//  the 1-, 2-, 3- and 4-interesting polygons in the picture below.
+// Example:
+// For n = 2, the output should be
+// solution(n) = 5;
+// For n = 3, the output should be
+// solution(n) = 13.
+// SOLUTION:
+const solution = (n) => {
+    // figure out pattern vv
+//               (n-1)^2  | n^2
+// n = 1; area: 1 = 0 + 1
+// n = 2; area: 5 = 1 + 4
+// n = 3; area: 13 = 4 + 9
+// n = 4; area: 25 = 9 + 16
+    // this returns the area of the given polygon
+    return Math.pow(n-1, 2) + Math.pow(n, 2)
+}
 
+console.log(solution(3)) // expected: 13
 
 
 
